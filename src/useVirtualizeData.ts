57dc39e3025e81830bd71a1b6a2c8bc7b8dc24a6
ALLWEB\taingch <chheangseng.taing@allweb.com.kh>
2025-02-05 15:50:28 +0700

    fix: edit code

--- src/useVirtualizeData.ts
+++ src/useVirtualizeData.ts
@@ -1,50 +1,51 @@
 import {useCallback, useMemo, useState} from 'react';
 
 export default function useVirtualizeData<T>({
-  data = [],
-  storeAmountOfPages = 2,
-  itemsPerPage = 30,
-}: {
-  data: T[];
-  itemsPerPage?: number;
-  storeAmountOfPages?: number;
+                                                 data = [],
+                                                 storeAmountOfPages = 2,
+                                                 itemsPerPage = 30,
+                                             }: {
+    data: T[];
+    itemsPerPage?: number;
+    storeAmountOfPages?: number;
 }) {
-  const [viewPages, setViewPages] = useState(() => {
-    return Array.from({
-      length: storeAmountOfPages,
-    }).map((_value, index) => index + 1);
-  });
+    const [viewPages, setViewPages] = useState(() => {
+        return Array.from({
+            length: storeAmountOfPages,
+        }).map((_value, index) => index + 1);
+    });
 
-  const totalPages = useMemo(() => {
-    return Math.ceil(data.length / itemsPerPage);
-  }, [data]);
+    const totalPages = useMemo(() => {
+        return Math.ceil(data.length / itemsPerPage);
+    }, [data, itemsPerPage]);
 
-  const paginatedData = useMemo(() => {
-    let result: T[] = [];
-    for (const page of viewPages) {
-      const startIndex = (page - 1) * itemsPerPage;
-      result = result.concat(data.slice(startIndex, startIndex + itemsPerPage));
-    }
-    return result;
-  }, [data, viewPages, itemsPerPage]);
+    const paginatedData = useMemo(() => {
+        let result: T[] = [];
+        for (const page of viewPages) {
+            const startIndex = (page - 1) * itemsPerPage;
+            result = result.concat(data.slice(startIndex, startIndex + itemsPerPage));
+        }
+        return result;
+    }, [data, viewPages, itemsPerPage]);
 
-  const canGoNext = viewPages[viewPages.length - 1] < totalPages;
-  const canGoBack = viewPages[0] > 1;
+    const canGoNext = viewPages[viewPages.length - 1] < totalPages;
+    const canGoBack = viewPages[0] > 1;
 
-  const goNext = useCallback(() => {
-    if (!canGoNext) return;
-    setViewPages(prevState => prevState.map(value => value + 1));
-  }, [canGoNext, setViewPages]);
+    const goNext = useCallback(() => {
+        if (!canGoNext) return;
+        setViewPages(prevState => prevState.map(value => value + 1));
+    }, [canGoNext, setViewPages]);
 
-  const goBack = useCallback(() => {
-    if (!canGoBack) return;
-    setViewPages(prevState => prevState.map(value => value - 1));
-  }, [canGoBack, setViewPages]);
-  const reset = useCallback(() => {
-    setViewPages(
-        Array.from({length: storeAmountOfPages}, (_, index) => index + 1),
-    );
-  }, [storeAmountOfPages]);
-  
-  return {data: paginatedData, goNext, goBack,reset};
+    const goBack = useCallback(() => {
+        if (!canGoBack) return;
+        setViewPages(prevState => prevState.map(value => value - 1));
+    }, [canGoBack, setViewPages]);
+
+    const reset = useCallback(() => {
+        setViewPages(
+            Array.from({length: storeAmountOfPages}, (_, index) => index + 1),
+        );
+    }, [storeAmountOfPages]);
+
+    return {data: paginatedData, goNext, goBack, reset};
 }
